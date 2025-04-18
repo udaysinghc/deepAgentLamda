a572fa44df2a2ea028b9fa1b836cef4d43b252eb
abacusUday <udaysingh@abacus.ai>
2025-04-18 15:08:52 +0530

    test

--- cucumber.js
+++ cucumber.js
@@ -2,13 +2,13 @@ module.exports = {
   default: {
     parallel: 1,
     format: [
-      'progress-bar',
-      ['html', 'reports/cucumber-report.html'],
-      ['json', 'reports/cucumber-report.json']
+      "progress-bar",
+      ["html", "reports/cucumber-report.html"],
+      ["json", "reports/cucumber-report.json"]
     ],
-    paths: ['features/**/*.feature'],
-    require: ['features/step_definitions/*.js', 'features/support/*.js'],
-    requireModule: ['@playwright/test'],
+    paths: ["features/**/*.feature"],
+    require: ["features/step_definitions/*.js", "features/support/*.js"],
+    requireModule: ["@playwright/test"],
     publishQuiet: true
   }
 };
